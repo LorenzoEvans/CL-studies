c45df03d1aab46196a1d516e5cc6ab55a05d795b
LorenzoEvans <lorenzo.evans94@gmail.com>
2019-12-09 21:39:44 -0500

    Going strong, doing the knowledge.

--- paip.lisp
+++ paip.lisp
@@ -14,16 +14,33 @@
 
 ; Often we distinguish between users and programmers-
 ; Users provide new input, whereas programmers define new operations,
-; or even new types of acceptable input.
+; or even new  types of acceptable input.
 
 
 ; Symbolic Computation:
 ; Besides numbers, we can represent letters, strings, and symbols.
 ; Non-atomic values are built by combining objects into lists.
 
-(print (append '("Pat" "Kim") '('"Robin" "Sandy")))
+; (print (append '("Jack" "Lyle") '("Eric" "Marc")))
 
+; (eval (append '("Pat" "Kim") '("Robin" "Sandy")))
 ; The above expression appends two lists of names.
 ; The quote mark blocks the evaluation of the following value,
 ; which keeps the strings Pat/Robin from being evaluated as functions,
-; (which is probably why wrapping append in eval kept erroring out.
+; Succinctly :: ' turns the following value into data.
+
+
+
+(print (append '(Pat Kim) (list '(Robin Sandy) '(John Q Public))))
+
+; About symbols
+
+; Lisp attaches no external significance to symbols, while we recognize the
+; above as names, Lisp makes no distinctions about symbols, so long as the
+; syntax is correct.
+
+; Symbols in lisp are *not* case sensitive. John == jOhN = jOHn.
+
+; A wide range of characters are acceptable for symbols.
+
+; 
